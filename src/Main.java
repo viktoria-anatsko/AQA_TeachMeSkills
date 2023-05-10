aa469836b2175ab9c3c47e1c0ec7513fc4c0be57
Tor-YA <69291928+Tor-YA@users.noreply.github.com>
2023-05-10 23:36:35 +0300

    Solution of homework2.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("""
+                    |    |
+                ____|____|____
+                    |    |
+                ____|____|____
+                    |    |
+                    |    |
+                """);
+
+        System.out.println("""
+                    *
+                   ***
+                  *****
+                 *******
+                *********""");
     }
 }
